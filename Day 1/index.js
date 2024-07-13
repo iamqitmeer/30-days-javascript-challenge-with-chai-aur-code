4e6cd0df087a7b6d589b195b6dbd09144ee3cf50
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-07-13 19:45:38 +0500

    First Day Complete

---
+++ Day 1/index.js
@@ -0,0 +1,101 @@
+// Day 1: Variables and Data Types
+
+// Tasks/Activities:
+// Activity 1: Variable Declaration
+// • Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+
+// var num = 120;
+// console.log(num);
+
+// • Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+
+// let str = "Muhammad Raza";
+// console.log(str);
+
+// Activity 2: Constant Declaration
+// ⚫ Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+// let booool = false;
+// console.log(booool);
+
+// Activity 3: Data Types
+// • Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the type of operator.
+
+// Number
+// let num = 10;
+// console.log(typeof(num));
+
+// String
+// let booool = false;
+// console.log(typeof booool);
+
+// Boolean
+// let str = "Muhammad Raza";
+// console.log(typeof str);
+
+// null
+
+// let nullVal = null
+// console.log(typeof nullVal);
+
+// Activity 4: Reassigning Variables.
+// ⚫ Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+
+// let val = "Qitmeer";
+// console.log("Before Reassigning " + val);
+
+// val = "Qitmeer Raza";
+// console.log("After Reassigning " + val);
+
+// Activity 5: Understanding const
+// Task 6: Try reassigning a variable declared with const and observe the error.
+
+// const val = "Qitmeer";
+// console.log("Before Reassigning " + val);
+
+// val = "Qitmeer Raza"; // Error - Assignment to constant variable.
+// console.log("After Reassigning " + val);
+
+// Feature Request:
+// 1. Variable Types Console Log: Write a script that declares variables of different data types and logs both the value and type of each variable to the console.
+
+// let num = 12
+// let str = "Raza"
+// let booool = true
+// let nullVal = null
+
+// console.log(num + " is a "+ typeof(num));
+// console.log(str + " is a "+ typeof(str));
+// console.log(booool + " is a "+ typeof(booool));
+// console.log(nullVal + " is a "+ typeof(nullVal));
+
+// 2. Reassignment Demo: Create a script that demonstrates the difference in behavior between let and const when it comes to reassignment.
+
+// var & let example
+
+// var val = "Qitmeer";
+// console.log("Before Reassigning " + val);
+
+// val = "Qitmeer Raza";
+// console.log("After Reassigning " + val);
+
+// let val = "Qitmeer";
+// console.log("Before Reassigning " + val);
+
+// val = "Qitmeer Raza";
+// console.log("After Reassigning " + val);
+
+// Const Example
+
+// const val = "Qitmeer";
+// console.log("Before Reassigning " + val);
+
+// val = "Qitmeer Raza"; Error ://: Assignment to constant variable.
+// console.log("After Reassigning " + val);
+
+// Achievement:
+// By the end of these activities, you will:
+// • Know how to declare variables using var, let, and const.
+// • Understand the different data types in JavaScript.
+// • Be able to use the typeof operator to identify the data type of a variable.
+// • Understand the concept of variable reassignment and the immutability of const variables.
